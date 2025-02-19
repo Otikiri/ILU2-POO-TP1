af41d366c56d082be92bcc26cfd4bfc65bb1af1b
Otikiri <sayvirakyuth@gmail.com>
2025-02-19 11:05:22 +0100

    exercice1 q4-5-6

--- src/villagegaulois/Village.java
+++ src/villagegaulois/Village.java
@@ -1,5 +1,7 @@
 package villagegaulois;
 
+import java.util.Iterator;
+
 import personnages.Chef;
 import personnages.Gaulois;
 
@@ -8,6 +10,7 @@ public class Village {
 	private Chef chef;
 	private Gaulois[] villageois;
 	private int nbVillageois = 0;
+	private Marche marche;
 
 	public Village(String nom, int nbVillageoisMaximum) {
 		this.nom = nom;
@@ -21,26 +24,56 @@ public class Village {
 	public void setChef(Chef chef) {
 		this.chef = chef;
 	}
-	
+
 	private static class Marche {
 
-		private int nbEtalTotal; 
-		private int nbEtalOcc=0; 
-		private Etal[] etals; 
-		
+		private int nbEtalTotal;
+		private int nbEtalOcc = 0;
+		private Etal[] etals;
+
 		public Marche(int nbEtTotal) {
 			nbEtalTotal = nbEtTotal;
 			this.etals = new Etal[nbEtalTotal];
-			
+
 		}
-		public void utiliserEtal(int indiceEtal,Gaulois vendeur,String produi , int nbProduit) {
-			if (nbEtalOcc==nbEtalTotal) {
-				System.out.println("Marche est rempli"); 
+
+		public void utiliserEtal(int indiceEtal, Gaulois vendeur, String produi, int nbProduit) {
+			if (nbEtalOcc == nbEtalTotal) {
+				System.out.println("Marche est rempli");
 				return;
 			}
 			etals[indiceEtal].occuperEtal(vendeur, produi, nbProduit);
-			nbEtalOcc++; 
-			return;
+			nbEtalOcc++;
+		}
+
+		public int trouverEtalLibre() {
+			for (int i = 0; i < etals.length; i++) {
+				if (!(etals[i].isEtalOccupe())) {
+					return i;
+				}
+			}
+			return -1;
+		}
+
+		public Etal[] trouverEtals(String produit) {
+			Etal[] etalProduit = new Etal[nbEtalOcc];
+			for (int i = 0; i < etalProduit.length; i++) {
+				for (int j = 0; j < etalProduit.length; j++) {
+					if (etals[j].contientProduit(produit)) {
+						etalProduit[i] = etals[j];
+					}
+				}
+			}
+			return etalProduit;
+		}
+		
+		public Etal trouverVendeur(Gaulois gaulois) {
+			for (int i = 0; i < etals.length; i++) {
+				if (etals[i].getVendeur().equals(gaulois)) {
+					return etals[i]; 
+				}
+			}
+			return null;
 		}
 	}
 
@@ -67,11 +100,9 @@ public class Village {
 	public String afficherVillageois() {
 		StringBuilder chaine = new StringBuilder();
 		if (nbVillageois < 1) {
-			chaine.append("Il n'y a encore aucun habitant au village du chef "
-					+ chef.getNom() + ".\n");
+			chaine.append("Il n'y a encore aucun habitant au village du chef " + chef.getNom() + ".\n");
 		} else {
-			chaine.append("Au village du chef " + chef.getNom()
-					+ " vivent les légendaires gaulois :\n");
+			chaine.append("Au village du chef " + chef.getNom() + " vivent les légendaires gaulois :\n");
 			for (int i = 0; i < nbVillageois; i++) {
 				chaine.append("- " + villageois[i].getNom() + "\n");
 			}
